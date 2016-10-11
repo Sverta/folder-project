3697fa094ac682d2bcf860b3df3cb29061c14c84
sbaidak <sbaidak@scnsoft.com>
2016-10-11 16:46:10 +0300

    add promise function

--- app/app.js
+++ app/app.js
@@ -1,6 +1,14 @@
 document.addEventListener("DOMContentLoaded",
     function loadTreeData() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '../tree.json', false);
 
+        xhr.onload = function() {
+                return new Promise(function(resolve) {
+
+                });
+        };
+        //xhr.send();
 });
 
 
