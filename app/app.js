3a79bcc36980b885473e69b1f03fba3229910d08
sbaidak <sbaidak@scnsoft.com>
2016-10-17 20:01:29 +0300

    last commit

--- app/app.js
+++ app/app.js
@@ -1,14 +1,22 @@
 document.addEventListener("DOMContentLoaded",
     function loadTreeData() {
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', '../tree.json', false);
+        xhr.open('GET', 'public/tree.json', true);
 
-        xhr.onload = function() {
+        xhr.onreadystatechange = function(e) {
+
                 return new Promise(function(resolve) {
+                        if (this.status == 200) {
+                                var response = JSON.parse(this.responseText);
 
+                        }
+                        else {
+
+                        }
                 });
+                xhr.send();
         };
-        //xhr.send();
+
 });
 
 
