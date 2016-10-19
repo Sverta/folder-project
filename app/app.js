c2200a1f2f728e6a1f2f5a7320db3f77c2e9dce2
sbaidak <sbaidak@scnsoft.com>
2016-10-19 17:18:42 +0300

    rm open data

--- app/app.js
+++ app/app.js
@@ -2,19 +2,29 @@ document.addEventListener("DOMContentLoaded",
     function loadTreeData() {
         var xhr = new XMLHttpRequest();
         xhr.open('GET', 'public/tree.json', true);
+            return new Promise(function(resolve) {
+                    if (this.status == 200) {
+                            //var response = JSON.parse(this.responseText);
+
+                    }
+                    else {
+
+                    }
+                    xhr.send()
+            }),
 
         xhr.onreadystatechange = function(e) {
+                //
+                // return new Promise(function(resolve) {
+                //         if (this.status == 200) {
+                //                 //var response = JSON.parse(this.responseText);
+                //
+                //         }
+                //         else {
+                //
+                //         }
+                // });
 
-                return new Promise(function(resolve) {
-                        if (this.status == 200) {
-                                var response = JSON.parse(this.responseText);
-
-                        }
-                        else {
-
-                        }
-                });
-                xhr.send();
         };
 
 });
