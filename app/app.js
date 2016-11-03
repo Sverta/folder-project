a2f5c49c84b21423aea58f27841fbc7c379247bb
sbaidak <sbaidak@scnsoft.com>
2016-11-03 19:45:24 +0300

    parse json file

--- app/app.js
+++ app/app.js
@@ -1,34 +1,33 @@
 document.addEventListener("DOMContentLoaded",
     function loadTreeData() {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'public/tree.json', true);
-            return new Promise(function(resolve) {
-                    if (this.status == 200) {
-                            //var response = JSON.parse(this.responseText);
+        var xhr = new XMLHttpRequest(); //создает новый объект
+            xhr.open('GET', 'public/tree.json', true);
+            xhr.send();
+            xhr.onreadystatechange = function() {
+                    if (this.readyState != 4) return;
+                    var jsonResponse = JSON.parse(xhr.responseText, function (key,value) {
+                            var folder = document.getElementById('folder');
+                            var link = "<a href='#'>"+xhr.responseText+"</a>";
+                            folder.innerHTML = "<ul><li>"+link+"</li></ul>";
+                            // for( var prop in this ) {
+                            //         if( this.hasOwnProperty( prop ) ) {
+                            //                 if( this[ prop ] === value )
+                            //                         return prop;
+                            //                 //console.log(prop);
+                            //         }
+                            // }
 
-                    }
-                    else {
+                               // if(key == 'name'){
+                                    //       //  return value;
+                                    //         console.log(value)
+                                    // }else if(key == 'children'){
+                                    //
+                                    // }
 
-                    }
-                    xhr.send()
-            }),
 
-        xhr.onreadystatechange = function(e) {
-                //
-                // return new Promise(function(resolve) {
-                //         if (this.status == 200) {
-                //                 //var response = JSON.parse(this.responseText);
-                //
-                //         }
-                //         else {
-                //
-                //         }
-                // });
-
-        };
-
-});
-
+                    });
+            };
+    });
 
 
 
